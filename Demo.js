98dbbf77fe871120e666be2ec392a330b7d2e7cf
yasindu1997 <yasindusathsaraoth@gmail.com>
2020-12-17 12:06:17 +0530

    changed array

---
+++ Demo.js
@@ -0,0 +1,13 @@
+var array = [
+    { id: 1, start: "2016-12-07T13:00:00", subject: "test1" },
+    { id: 2, start: "2016-12-07T09:00:00", subject: "test2" },
+    { id: 3, start: "2016-12-07T10:00:00", subject: "test3" },
+    { id: 4, start: "2016-12-07T07:00:00", subject: "test4" },
+    { id: 5, start: "2016-12-07T14:00:00", subject: "test5" }
+];
+
+array.sort(function (a, b) {
+    return a.start.localeCompare(b.start);
+});
+
+console.log(array);
